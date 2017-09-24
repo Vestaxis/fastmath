3e6d58bc6aa10c9a9552d2e9e9f4fea0117f9fd1
stephen <stephen@msi>
2017-09-24 16:10:34 -0500

    look in /usr/lib first

--- src/fastmath/NativeUtils.java
+++ src/fastmath/NativeUtils.java
@@ -15,95 +15,103 @@ import java.io.OutputStream;
 public class NativeUtils
 {
 
-  private static Boolean cachedTerribleExcuseForAnOperatingSystemIndicator;
+	private static Boolean cachedTerribleExcuseForAnOperatingSystemIndicator;
 
-  /**
-   * Private constructor - this class will never be instanced
-   */
-  private NativeUtils()
-  {
-  }
+	/**
+	 * Private constructor - this class will never be instanced
+	 */
+	private NativeUtils()
+	{
+	}
 
-  /**
-   * 
-   * @return true if the "os.name" Java sytem property contains the string
-   *         "Windows"
-   */
-  public static boolean isTerribleExcuseForAnOperatingSystem()
-  {
-    if ( cachedTerribleExcuseForAnOperatingSystemIndicator != null )
-    {
-      return cachedTerribleExcuseForAnOperatingSystemIndicator;
-    }
-    cachedTerribleExcuseForAnOperatingSystemIndicator = System.getProperty( "os.name" ).contains( "Windows" );
-    return cachedTerribleExcuseForAnOperatingSystemIndicator;
-  }
+	/**
+	 * 
+	 * @return true if the "os.name" Java sytem property contains the string
+	 *         "Windows"
+	 */
+	public static boolean isTerribleExcuseForAnOperatingSystem()
+	{
+		if (cachedTerribleExcuseForAnOperatingSystemIndicator != null)
+		{
+			return cachedTerribleExcuseForAnOperatingSystemIndicator;
+		}
+		cachedTerribleExcuseForAnOperatingSystemIndicator = System.getProperty("os.name").contains("Windows");
+		return cachedTerribleExcuseForAnOperatingSystemIndicator;
+	}
 
-  /**
-   * Loads library from current JAR archive
-   * 
-   * The file from JAR is copied into system temporary directory and then
-   * loaded. The temporary file is deleted after exiting. Method uses String as
-   * filename because the pathname is "abstract", not system-dependent.
-   * 
-   * @param filename
-   *          The filename inside JAR as absolute path (beginning with '/'),
-   *          e.g. /package/File.ext
-   * @throws IOException
-   *           If temporary file creation or read/write operation fails
-   * @throws IllegalArgumentException
-   *           If source file (param path) does not exist
-   * @throws IllegalArgumentException
-   *           If the path is not absolute or if the filename is shorter than
-   *           three characters (restriction of
-   *           {@see File#createTempFile(java.lang.String, java.lang.String)}).
-   */
-  public static void loadLibraryFromClasspath( String libraryPathExcludingFilenameExtension ) throws IOException
-  {
-    String filenameExtension = isTerribleExcuseForAnOperatingSystem() ? "dll" : "so";
-    File temp = File.createTempFile( "library", filenameExtension );
-    temp.deleteOnExit();
+	/**
+	 * Loads library from current JAR archive
+	 * 
+	 * The file from JAR is copied into system temporary directory and then loaded.
+	 * The temporary file is deleted after exiting. Method uses String as filename
+	 * because the pathname is "abstract", not system-dependent.
+	 * 
+	 * @param filename
+	 *            The filename inside JAR as absolute path (beginning with '/'),
+	 *            e.g. /package/File.ext
+	 * @throws IOException
+	 *             If temporary file creation or read/write operation fails
+	 * @throws IllegalArgumentException
+	 *             If source file (param path) does not exist
+	 * @throws IllegalArgumentException
+	 *             If the path is not absolute or if the filename is shorter than
+	 *             three characters (restriction of
+	 *             {@see File#createTempFile(java.lang.String, java.lang.String)}).
+	 */
+	public static void loadLibraryFromClasspath(String libraryPathExcludingFilenameExtension) throws IOException
+	{
+		String filenameExtension = isTerribleExcuseForAnOperatingSystem() ? "dll" : "so";
+		File temp = File.createTempFile("library", filenameExtension);
+		temp.deleteOnExit();
 
-    if ( !temp.exists() )
-    {
-      throw new FileNotFoundException( "Failed to open temporary file at " + temp.getAbsolutePath() );
-    }
+		if (!temp.exists())
+		{
+			throw new FileNotFoundException("Failed to open temporary file at " + temp.getAbsolutePath());
+		}
 
-    byte[] buffer = new byte[1024];
-    int readBytes;
+		byte[] buffer = new byte[1024];
+		int readBytes;
+		try
+		{
+			System.loadLibrary("/usr/lib/" + libraryPathExcludingFilenameExtension + "." + filenameExtension);
+			return;
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 
-    InputStream is = ClassLoader.getSystemResourceAsStream( libraryPathExcludingFilenameExtension + "." + filenameExtension );
-    if ( is == null )
-    {
-      throw new FileNotFoundException( "File " + libraryPathExcludingFilenameExtension + " was not found by the class loader'." );
-    }
+		InputStream is = ClassLoader
+				.getSystemResourceAsStream(libraryPathExcludingFilenameExtension + "." + filenameExtension);
+		if (is == null)
+		{
+			throw new FileNotFoundException(
+					"File " + libraryPathExcludingFilenameExtension + " was not found by the class loader'.");
+		}
 
-    OutputStream os = new FileOutputStream( temp );
-    try
-    {
-      while (( readBytes = is.read( buffer ) ) != -1)
-      {
-        os.write( buffer, 0, readBytes );
-      }
-    }
-    finally
-    {
-      os.close();
-      is.close();
-    }
+		OutputStream os = new FileOutputStream(temp);
+		try
+		{
+			while ((readBytes = is.read(buffer)) != -1)
+			{
+				os.write(buffer, 0, readBytes);
+			}
+		} finally
+		{
+			os.close();
+			is.close();
+		}
 
-    System.load( temp.getAbsolutePath() );
-  }
+		System.load(temp.getAbsolutePath());
+	}
 
-  protected static void loadNativeFastmathLibrary()
-  {
-    try
-    {
-      loadLibraryFromClasspath( "libfastmath" );
-    }
-    catch( IOException e )
-    {
-      throw new RuntimeException( e.getMessage(), e );
-    }
-  }
+	protected static void loadNativeFastmathLibrary()
+	{
+		try
+		{
+			loadLibraryFromClasspath("libfastmath");
+		} catch (IOException e)
+		{
+			throw new RuntimeException(e.getMessage(), e);
+		}
+	}
 }
