5301eb4f5c4af697e0507d74ecaed1bbaf31d28f
stephen <no@where.com>
2017-09-26 14:33:17 -0500

    ...

--- src/stochastic/processes/hawkes/ExponentialHawkesProcess.java
+++ src/stochastic/processes/hawkes/ExponentialHawkesProcess.java
@@ -13,214 +13,222 @@ import fastmath.Vector;
 public abstract class ExponentialHawkesProcess
 {
 
-	protected abstract double α(int j);
+  protected abstract double α(int j);
 
-	protected abstract double β(int j);
+  protected abstract double β(int j);
 
-	public static void addSeriesToChart(XYChart chart, String name, Vector X, Vector Y)
-	{
-		chart.addSeries(name, X.toPrimitiveArray(), Y.toPrimitiveArray());
-	}
+  public static void addSeriesToChart(XYChart chart, String name, Vector X, Vector Y)
+  {
+    chart.addSeries(name, X.toPrimitiveArray(), Y.toPrimitiveArray());
+  }
 
-	public Vector T;
-	protected boolean recursive = false;
+  public Vector T;
+  protected boolean recursive = false;
 
-	public ExponentialHawkesProcess()
-	{
-		super();
-	}
+  public ExponentialHawkesProcess()
+  {
+    super();
+  }
 
-	public double getBranchingRatio()
-	{
-		return sum(i -> α(i) / β(i), 0, getOrder() - 1);
-	}
+  public double getBranchingRatio()
+  {
+    return sum(i -> α(i) / β(i), 0, getOrder() - 1);
+  }
 
-	public abstract int getOrder();
+  public abstract int getOrder();
 
-	public abstract double Z();
+  public abstract double Z();
 
-	/**
-	 * intensity function
-	 * 
-	 * @param t
-	 * 
-	 * @return intensity at time t
-	 */
-	public final double λ(double t)
-	{
-		return T.stream().filter(s -> s < t).map(s -> ψ(t - s)).sum();
-	}
+  /**
+   * intensity function
+   * 
+   * @param t
+   * 
+   * @return intensity at time t
+   */
+  public final double λ(double t)
+  {
+    return T.stream().filter(s -> s < t).map(s -> ψ(t - s)).sum();
+  }
 
-	/**
-	 * kernel function
-	 * 
-	 * @param t
-	 * @return
-	 */
-	public final double ψ(double t)
-	{
-		return sum(i -> α(i) * exp(-β(i) * t), 0, getOrder() - 1) / Z();
-	}
+  /**
+   * kernel function
+   * 
+   * @param t
+   * @return
+   */
+  public final double ψ(double t)
+  {
+    return sum(i -> α(i) * exp(-β(i) * t), 0, getOrder() - 1) / Z();
+  }
 
-	/**
-	 * integrated kernel function
-	 * 
-	 * @param t
-	 * @return
-	 */
-	public double iψ(double t)
-	{
-		return sum(i -> -(α(i) / β(i)) * (exp(-β(i) * t) - 1), 0, getOrder() - 1) / Z();
-	}
+  /**
+   * integrated kernel function
+   * 
+   * @param t
+   * @return
+   */
+  public double iψ(double t)
+  {
+    return sum(i -> -(α(i) / β(i)) * (exp(-β(i) * t) - 1), 0, getOrder() - 1) / Z();
+  }
 
-	protected double evolveλ(double dt, double[] S)
-	{
-		double λ = 0;
-		for (int j = 0; j < getOrder(); j++)
-		{
-			S[j] = exp(-β(j) * dt) * (1 + S[j]);
-			λ += α(j) * S[j];
-		}
-		return λ / Z();
-	}
+  protected double evolveλ(double dt, double[] S)
+  {
+    double λ = 0;
+    for (int j = 0; j < getOrder(); j++)
+    {
+      S[j] = exp(-β(j) * dt) * (1 + S[j]);
+      λ += α(j) * S[j];
+    }
+    return λ / Z();
+  }
 
-	protected double getλ0()
-	{
-		return 0;
-	}
+  protected double getλ0()
+  {
+    return 0;
+  }
 
-	protected double evolveΛ(double dt, double[] A)
-	{
-		double Λ = dt * getλ0();
-		for (int j = 0; j < getOrder(); j++)
-		{
-			double a = α(j);
-			double b = β(j);
-			A[j] = 1 + exp(-b * dt) * A[j];
-			Λ += (a / b) * (1 - exp(-b * dt)) * A[j];
-		}
-		return Λ;
-	}
+  protected double evolveΛ(double dt, double[] A)
+  {
+    double Λ = dt * getλ0();
+    for (int j = 0; j < getOrder(); j++)
+    {
+      double a = α(j);
+      double b = β(j);
+      A[j] = 1 + exp(-b * dt) * A[j];
+      Λ += (a / b) * (1 - exp(-b * dt)) * A[j];
+    }
+    return Λ;
+  }
 
-	protected Vector calculateCompensator(final int n)
-	{
-		Vector durations = T.diff();
+  protected Vector calculateRecursiveCompensator(final int n)
+  {
+    Vector durations = T.diff();
 
-		double A[] = new double[getOrder()];
-		Vector compensator = new Vector(n);
-		for (int i = 0; i < n - 1; i++)
-		{
-			double dt = durations.get(i);
-			double secondSum = evolveΛ(dt, A);
-			compensator.set(i, secondSum);
-		}
-		return compensator;
-	}
+    double A[] = new double[getOrder()];
+    Vector compensator = new Vector(n);
+    for (int i = 0; i < n; i++)
+    {
+      double dt = durations.get(i);
+      double secondSum = evolveΛ(dt, A);
+      compensator.set(i, secondSum);
+    }
+    return compensator;
+  }
 
-	/**
-	 * 
-	 * @param T
-	 * @param deterministicIntensity
-	 * @param lambda
-	 * @param alpha
-	 * @param bη
-	 * @return Pair<logLik,E[Lambda]>
-	 */
-	public double logLik()
-	{
-		double tn = T.getRightmostValue();
-		double ll = tn - T.getLeftmostValue();
-		final int n = T.size();
+  /**
+   * 
+   * @param T
+   * @param deterministicIntensity
+   * @param lambda
+   * @param alpha
+   * @param bη
+   * @return Pair<logLik,E[Lambda]>
+   */
+  public double logLik()
+  {
+    double tn = T.getRightmostValue();
+    double ll = tn - T.getLeftmostValue();
+    final int n = T.size();
 
-		if (recursive)
-		{
-			double A[] = new double[getOrder()];
-			double S[] = new double[getOrder()];
-			for (int i = 1; i < n; i++)
-			{
-				double t = T.get(i);
-				double dt = t - T.get(i - 1);
-				double λ = evolveλ(dt, S);
-				double Λ = evolveΛ(dt, A);
+    if (recursive)
+    {
+      double A[] = new double[getOrder()];
+      double S[] = new double[getOrder()];
+      for (int i = 1; i < n; i++)
+      {
+        double t = T.get(i);
+        double dt = t - T.get(i - 1);
+        double λ = evolveλ(dt, S);
+        double Λ = evolveΛ(dt, A);
 
-				// double Λ = sum(j -> ( α(j) / β(j) ) * (exp(-β(j) * (tn - t)) - 1), 0, M);
+        // double Λ = sum(j -> ( α(j) / β(j) ) * (exp(-β(j) * (tn - t)) - 1), 0, M);
 
-				if (λ > 0)
-				{
-					ll += log(λ);
-				}
+        if (λ > 0)
+        {
+          ll += log(λ);
+        }
 
-				ll -= Λ;
+        ll -= Λ;
 
-			}
-		}
-		else
-		{
-			for (int i = 1; i < n; i++)
-			{
-				double thist = T.get(i);
-				ll += log(λ(thist)) - Λ(i);
-			}
-			out.println("LL{" + getParamString() + "}=" + ll);
-			if (Double.isNaN(ll))
-			{
-				ll = Double.NEGATIVE_INFINITY;
-			}
-		}
-		return ll;
+      }
+    }
+    else
+    {
+      for (int i = 1; i < n; i++)
+      {
+        double thist = T.get(i);
+        ll += log(λ(thist)) - Λ(i);
+      }
+      out.println("LL{" + getParamString() + "}=" + ll);
+      if (Double.isNaN(ll))
+      {
+        ll = Double.NEGATIVE_INFINITY;
+      }
+    }
+    return ll;
 
-	}
+  }
 
-	public abstract String getParamString();
+  public abstract String getParamString();
 
-	/**
-	 * n-th compensated point
-	 * 
-	 * @param i
-	 *            >= 1 and <= n
-	 * @return sum(k -> iψ(T.get(i + 1) - T.get(k)) - iψ(T.get(i) - T.get(k)), 0,
-	 *         i-1)
-	 */
-	protected double Λ(int i)
-	{
+  /**
+   * n-th compensated point
+   * 
+   * @param i
+   *          >= 1 and <= n
+   * @return sum(k -> iψ(T.get(i + 1) - T.get(k)) - iψ(T.get(i) - T.get(k)), 0,
+   *         i-1)
+   */
+  protected double Λ(int i)
+  {
 
-		double sum = sum(k -> {
-			double t0 = T.get(i) - T.get(k);
-			double t1 = T.get(i - 1) - T.get(k);
-			return iψ(t0) - iψ(t1);
-		}, 0, i - 1);
-		return sum;
-	}
+    double sum = sum(k -> {
+      double t0 = T.get(i) - T.get(k);
+      double t1 = T.get(i - 1) - T.get(k);
+      return iψ(t0) - iψ(t1);
+    }, 0, i - 1);
+    return sum;
+  }
 
-	/**
-	 * The random variable defined by 1-exp(-ξ(i)-ξ(i-1)) indicates a better fit the
-	 * more uniformly distributed it is.
-	 * 
-	 * 
-	 * @see UniformRealDistribution on [0,1]
-	 * 
-	 * @param times
-	 * 
-	 * @return ξ
-	 */
-	public Vector Λ()
-	{
-		final int n = T.size();
+  public double totalΛ()
+  {
+    double tn = T.getRightmostValue();
 
-		if (recursive)
-		{
-			return calculateCompensator(n);
-		}
-		else
-		{
-			Vector compensator = new Vector(n);
-			for (int i = 1; i < n; i++)
-			{
-				compensator.set(i, Λ(i));
-			}
-			return compensator;
-		}
+    return tn * getλ0()
+        + sum(i -> sum(j -> (α(j) / β(j)) * (1 - exp(-β(j) * (tn - T.get(i))) ), 0, getOrder() - 1), 0, T.size() - 1);
+  }
 
-	}
+  /**
+   * The random variable defined by 1-exp(-ξ(i)-ξ(i-1)) indicates a better fit the
+   * more uniformly distributed it is.
+   * 
+   * 
+   * @see UniformRealDistribution on [0,1]
+   * 
+   * @param times
+   * 
+   * @return ξ
+   */
+  public Vector Λ()
+  {
+    final int n = T.size() - 1;
+
+    if (recursive)
+    {
+      return calculateRecursiveCompensator(n);
+    }
+    else
+    {
+      Vector compensator = new Vector(n);
+      for (int i = 0; i < n; i++)
+      {
+        compensator.set(i, Λ(i + 1));
+      }
+      return compensator;
+    }
+
+  }
 
 }
--- src/stochastic/processes/hawkes/StandardExponentialHawkesProcessTest.java
+++ src/stochastic/processes/hawkes/StandardExponentialHawkesProcessTest.java
@@ -1,5 +1,6 @@
 package stochastic.processes.hawkes;
 
+import static java.lang.Math.pow;
 import static java.lang.System.out;
 
 import java.io.IOException;
@@ -19,21 +20,37 @@ public class StandardExponentialHawkesProcessTest extends TestCase
 		StandardExponentialHawkesProcess process = new StandardExponentialHawkesProcess(0.1, α, β);
 		Vector data = MatFile.loadMatrix("/data/SPY.mat", "SPY").col(0);
 		int midpoint = data.size() / 2;
-		data = data.slice(midpoint - 250, midpoint + 250);
+		data = data.slice(midpoint - 50, midpoint + 50);
 		process.T = data;
+		
+		double totalΛ = process.totalΛ();
+		out.println( "totalΛ=" + totalΛ );
+		
 		// process.estimateParameters(15);
 		process.recursive = true;
 		Vector recursiveComp = process.Λ();
 		double recursiveMean = recursiveComp.mean();
 		double recursiveVar = recursiveComp.variance();
+		out.println( "recursive Λ=" + recursiveComp ); 
+		out.println( "recursive Λ size " + recursiveComp.size() );
 		out.println("recursive mean=" + recursiveMean);
 		out.println("recursive var=" + recursiveVar);
 		process.recursive = false;
 		Vector comp = process.Λ();
 		double mean = comp.mean();
 		double var = comp.variance();
+		out.println( "comp Λ=" + comp );
+		out.println( "Λ size " + recursiveComp.size() );
 		out.println("mean=" + mean);
 		out.println("var=" + var);
+		
+		double compSum = comp.sum();
+    out.println( "Σcomp = " + compSum );
+    
+		out.println( "Σrecursivecomp = " + recursiveComp.sum() );
+		
+		assertEquals( totalΛ, compSum, pow( 10, -14 ) );
+		
 		assertEquals(mean, recursiveMean);
 		assertEquals(var, recursiveVar);
 
