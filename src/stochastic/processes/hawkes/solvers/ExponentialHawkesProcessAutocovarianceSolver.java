39695471395ee9b97fb80d38e4c6a49558110ffb
stephen <no@where.com>
2017-10-23 00:48:42 -0500

    must sleep... stephen@bb:~$ covsolver P=2 first=2*alpha[1]^2*beta[2]+2*alpha[1]*alpha[2]*beta[1]+2*alpha[1]*alpha[2]*beta[2]-4*alpha[1]*beta[1]*beta[2]-2*alpha[1]*beta[2]^2+2*alpha[2]^2*beta[1]-2*alpha[2]*beta[1]^2-4*alpha[2]*beta[1]*beta[2]+2*beta[1]^2*beta[2]+2*beta[1]*beta[2]^2 second=2*alpha[1]^2*beta[2]+2*alpha[1]*alpha[2]*beta[1]+2*alpha[1]*alpha[2]*beta[2]-4*alpha[1]*beta[1]*beta[2]-2*alpha[1]*beta[2]^2+2*alpha[2]^2*beta[1]-2*alpha[2]*beta[1]^2-4*alpha[2]*beta[1]*beta[2]+2*beta[1]^2*beta[2]+2*beta[1]*beta[2]^2 Goodbye stephen@bb:~

--- src/stochastic/processes/hawkes/solvers/ExponentialHawkesProcessAutocovarianceSolver.java
+++ src/stochastic/processes/hawkes/solvers/ExponentialHawkesProcessAutocovarianceSolver.java
@@ -1,10 +1,13 @@
 package stochastic.processes.hawkes.solvers;
 
 import static java.lang.String.format;
+import static java.lang.System.out;
 
 import com.maplesoft.externalcall.MapleException;
+import com.maplesoft.openmaple.Algebraic;
 import com.maplesoft.openmaple.Engine;
 import com.maplesoft.openmaple.EngineCallBacksDefault;
+import com.maplesoft.openmaple.List;
 
 public class ExponentialHawkesProcessAutocovarianceSolver
 {
@@ -17,14 +20,21 @@ public class ExponentialHawkesProcessAutocovarianceSolver
     a[0] = "java";
     String libDir = "c:\\\\research";
     t = new Engine(a, new EngineCallBacksDefault(), null, null);
-    t.evaluate(format("libname:=libname,\"%s\";", libDir));
+    t.evaluate(format("libname:=libname,\"%s\":", libDir));
     t.evaluate("with(cl):");
     t.evaluate("lcovar:=proc (P) options operator, arrow; (sum(alpha[j]*(lambda+mustar(beta[j]))/(beta[j]+s), j = 1 .. P))/(1-sum(alpha[j]/(beta[j]+s), j = 1 .. P)) end proc:");
     t.evaluate("lcovareq:=proc (k, P) options operator, arrow; eval(mustar(s) = lcovar(P), s = beta[k]) end proc:"); 
     t.evaluate("lcovarsol:=proc (P) options operator, arrow; solve(cl:-toset(cl:-flist(proc (k) options operator, arrow; lcovareq(k, P) end proc, 1 .. P)), cl:-toset(cl:-flist(proc (k) options operator, arrow; mustar(beta[k]) end proc, 1 .. P))) end proc:");  
         
-    t.evaluate("lcovarsol(1);");
-    t.evaluate("lcovarsol(2);");
-    System.out.println("Done\n");
+    //t.evaluate("lcovarsol(1);");
+    //t.evaluate("lcovarsol(2);");
+    List solutions = (List) t.evaluate("map(x->denom(op(2,x)),tolist(lcovarsol(2))):");
+    Algebraic first = solutions.select(1);
+    Algebraic second = solutions.select(2);
+    out.println( "P=" + solutions.length() );
+    out.println( "first=" + first );
+    out.println( "second=" + second );
+    
+    System.out.println("Goodbye");
   }
 }
