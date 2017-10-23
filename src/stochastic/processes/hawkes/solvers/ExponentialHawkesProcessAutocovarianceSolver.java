ae5d8efcc3778543c10dcde67e9e2c0b4c0e45c6
stephen <no@where.com>
2017-10-23 11:07:50 -0500

    save work

--- src/stochastic/processes/hawkes/solvers/ExponentialHawkesProcessAutocovarianceSolver.java
+++ src/stochastic/processes/hawkes/solvers/ExponentialHawkesProcessAutocovarianceSolver.java
@@ -23,18 +23,25 @@ public class ExponentialHawkesProcessAutocovarianceSolver
     t.evaluate(format("libname:=libname,\"%s\":", libDir));
     t.evaluate("with(cl):");
     t.evaluate("lcovar:=proc (P) options operator, arrow; (sum(alpha[j]*(lambda+mustar(beta[j]))/(beta[j]+s), j = 1 .. P))/(1-sum(alpha[j]/(beta[j]+s), j = 1 .. P)) end proc:");
-    t.evaluate("lcovareq:=proc (k, P) options operator, arrow; eval(mustar(s) = lcovar(P), s = beta[k]) end proc:"); 
-    t.evaluate("lcovarsol:=proc (P) options operator, arrow; solve(cl:-toset(cl:-flist(proc (k) options operator, arrow; lcovareq(k, P) end proc, 1 .. P)), cl:-toset(cl:-flist(proc (k) options operator, arrow; mustar(beta[k]) end proc, 1 .. P))) end proc:");  
-        
-    //t.evaluate("lcovarsol(1);");
-    //t.evaluate("lcovarsol(2);");
+    t.evaluate("lcovareq:=proc (k, P) options operator, arrow; eval(mustar(s) = lcovar(P), s = beta[k]) end proc:");
+    t.evaluate("lcovarsol:=proc (P) options operator, arrow; solve(cl:-toset(cl:-flist(proc (k) options operator, arrow; lcovareq(k, P) end proc, 1 .. P)), cl:-toset(cl:-flist(proc (k) options operator, arrow; mustar(beta[k]) end proc, 1 .. P))) end proc:");
+
+    // t.evaluate("lcovarsol(1);");
+    // t.evaluate("lcovarsol(2);");
     List solutions = (List) t.evaluate("map(x->denom(op(2,x)),tolist(lcovarsol(2))):");
     Algebraic first = solutions.select(1);
     Algebraic second = solutions.select(2);
-    out.println( "P=" + solutions.length() );
-    out.println( "first=" + first );
-    out.println( "second=" + second );
-    
+    String firstSol = replaceChars(first.toString());
+    // String ass = firstSol.replace("beta", "β");
+    out.println("P=" + solutions.length());
+    out.println("first=" + firstSol);
+    out.println("second=" + replaceChars(second.toString()));
+
     System.out.println("Goodbye");
+  }
+
+  public static String replaceChars(String first)
+  {
+    return first.replace("beta", "B").replace("alpha", "A");
   }
 }
